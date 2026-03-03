e853778ebc8443aa39f75b5562904e5d240cfa82
bmisarts <gilleskemgoum@gmail.com>
2026-03-03 19:22:55 +0100

    petits ajustement des formulaires

--- src/app/features/public-event/public-event.routes.ts
+++ src/app/features/public-event/public-event.routes.ts
@@ -1,8 +1,6 @@
 import { Routes } from '@angular/router';
 import { EventRegisterComponent } from './pages/event-register/event-register.component';
-import { EventConfirmationComponent } from './pages/event-confirmation/event-confirmation.component';
 
 export const PublicEventRoutes: Routes = [
   { path: ':slug', component: EventRegisterComponent },
-  { path: ':slug/confirmation', component: EventConfirmationComponent },
 ];
